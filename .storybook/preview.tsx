a7eb0df16edfe69330aea06fb1a59ccdc5f8c893
Jason Belmonti <jasonbelmonti@gmail.com>
2026-03-06 15:39:44 -0600

    Implement foundation theme system

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,12 @@
 import type { Preview } from "@storybook/react-webpack5";
 
 import "antd/dist/reset.css";
+import "@fontsource/azeret-mono/400.css";
+import "@fontsource/azeret-mono/500.css";
+import "@fontsource/oxanium/500.css";
+import "@fontsource/oxanium/600.css";
+import "@fontsource/oxanium/700.css";
+import "../src/styles/theme.css";
 import "../src/styles/storybook.css";
 
 import { AntdThemeProvider } from "../src/providers/AntdThemeProvider";
